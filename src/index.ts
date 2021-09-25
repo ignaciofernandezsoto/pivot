81bcca0cab7182708869c8c5f8ee401765f8c19d
Nacho Soto <isoto@momentfeed.com>
2021-09-25 03:55:11 -0300

    🧹 Tidying code

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,24 @@
-import {Client} from "transmission-api";
 import TelegramBot from "node-telegram-bot-api";
 
 import dotenv from "dotenv";
+import {TorrentService} from "./service/torrent/torrent.service";
 
 dotenv.config();
 
 const telegramToken = process.env.TELEGRAM_BOT_TOKEN!;
 const bot = new TelegramBot(telegramToken, {polling: true});
 
-const transmissionClient = new Client("", {
-    username: process.env.TRANSMISSION_USERNAME,
-    password: process.env.TRANSMISSION_PASSWORD,
-});
-
 const whitelistedUsers = process.env.WHITELISTED_USERS!.split(' ').map(u => parseInt(u));
-
-const statePerStatusNumber = {
-    '0': "⏸ Stopped",
-    '1': "⌛🕵️ Queued to check files",
-    '2': "🕵️ Checking files",
-    '3': "⌛▶️ Queued to download",
-    '4': "▶️ Downloading",
-    '5': "⌛🌱 Queued to seed",
-    '6': "🌱 Seeding",
-    '7': "🤷‍♂️ Can't find peers",
-}
-
-const numberFormat = new Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 2
-});
 
 bot.onText(/\/torrents$/, async (msg) => {
     const chatId = msg.chat.id;
     if (!whitelistedUsers.includes(chatId)) return;
 
     try {
-        const torrents = await transmissionClient.getAllTorrents();
-
-        if (torrents.length && !Array.isArray(torrents[0])) {
-            await bot.sendMessage(
-                chatId,
-                `🔢 ${torrents.length} torrents
-                ${torrents.map(t => `      
-🆔 ${t.id}
-🏷 ${t.name}
-${(statePerStatusNumber as any)[t.status.toString()]}
-🕒 ${t.eta > 0 ? etaToTimeString(t.eta) : '00:00:00'}
-📏 ${numberFormat.format(t.totalSize / (1024 * 1024 * 1024))} GB
-🏁 ${numberFormat.format(t.percentDone * 100)}%
-🚄 ${numberFormat.format(t.rateDownload / 1000000)} MiB/s
-            `).join(
-``
-                )}`
-            );
-        } else {
-            await bot.sendMessage(
-                chatId,
-                '🔢 No torrents'
-            );
-        }
+        await bot.sendMessage(
+            chatId,
+            await TorrentService.getAllAsMessage()
+        );
     } catch (e) {
         console.log(e);
         await bot.sendMessage(
@@ -75,13 +35,9 @@ bot.onText(/\/torrents add (.+)/, async (msg, match) => {
     const magnetUri = match![1];
 
     try {
-        const torrent = await transmissionClient.addTorrent(magnetUri);
         await bot.sendMessage(
             chatId,
-            `
-🆔 ${(torrent as any)["torrent-added"].id}
-🏷 ${(torrent as any)["torrent-added"].name}
-            `
+            await TorrentService.add(magnetUri)
         );
     } catch (e) {
         console.log(e);
@@ -99,10 +55,9 @@ bot.onText(/\/torrents delete (.+)/, async (msg, match) => {
     const id = parseInt(match![1]);
 
     try {
-        await transmissionClient.removeTorrent(id, true);
         await bot.sendMessage(
             chatId,
-            `🆔 ${id} deleted`
+            await TorrentService.deleteById(id)
         );
     } catch (e) {
         console.log(e);
@@ -112,9 +67,3 @@ bot.onText(/\/torrents delete (.+)/, async (msg, match) => {
         );
     }
 });
-
-const etaToTimeString = (eta: number) => {
-    const date = new Date(0);
-    date.setSeconds(eta);
-    return date.toISOString().substr(11, 8);
-}
---
+++ src/service/torrent/torrent.service.ts
@@ -0,0 +1,75 @@
+import {Client, ITorrent, Torrent} from "transmission-api";
+
+const transmissionClient = new Client("", {
+    username: process.env.TRANSMISSION_USERNAME,
+    password: process.env.TRANSMISSION_PASSWORD,
+});
+
+const statePerStatusNumber = {
+    '0': "⏸ Stopped",
+    '1': "⌛🕵️ Queued to check files",
+    '2': "🕵️ Checking files",
+    '3': "⌛▶️ Queued to download",
+    '4': "▶️ Downloading",
+    '5': "⌛🌱 Queued to seed",
+    '6': "🌱 Seeding",
+    '7': "🤷‍♂️ Can't find peers",
+}
+
+const numberFormat = new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: 2
+});
+
+const getAllAsMessage = async () => {
+    const torrents = await transmissionClient.getAllTorrents();
+
+    if (torrents.length && !Array.isArray(torrents[0])) {
+        return `${torrentsAmountTitle(torrents.length)}
+                ${torrents.map(t => torrentDataToDescription(t)).join(
+            ``
+        )}`
+    } else {
+        return noTorrents;
+    }
+}
+
+const add = async (magnetUri: string) => {
+    const torrent = await transmissionClient.addTorrent(magnetUri);
+    return addedTorrentDataToDescription(torrent);
+}
+
+const deleteById = async (id: number) => {
+    await transmissionClient.removeTorrent(id, true);
+    return `🆔 ${id} deleted`
+};
+
+const torrentDataToDescription = (t: Torrent) => `
+🆔 ${t.id}
+🏷 ${t.name}
+${(statePerStatusNumber as any)[t.status.toString()]}
+🕒 ${t.eta > 0 ? etaToTimeString(t.eta) : '00:00:00'}
+📏 ${numberFormat.format(t.totalSize / (1024 * 1024 * 1024))} GB
+🏁 ${numberFormat.format(t.percentDone * 100)}%
+🚄 ${numberFormat.format(t.rateDownload / 1000000)} MiB/s
+`
+
+const addedTorrentDataToDescription = (torrent: ITorrent) => `
+🆔 ${(torrent as any)["torrent-added"].id}
+🏷 ${(torrent as any)["torrent-added"].name}
+`
+
+const torrentsAmountTitle = (amount: number) => `🔢 ${amount} torrents`
+
+const noTorrents = '🔢 No torrents'
+
+const etaToTimeString = (eta: number) => {
+    const date = new Date(0);
+    date.setSeconds(eta);
+    return date.toISOString().substr(11, 8);
+}
+
+export const TorrentService = {
+    getAllAsMessage,
+    add,
+    deleteById,
+}
